09585907fa4117361afa80f0ca8df3289d80fd85
jeffrey <jeffreyon11@gmail.com>
2023-03-24 11:37:01 -0700

    library setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import useVerificationHook from "./lib/useVerificationCode";
+import type { InputState } from "./lib/useVerificationCode";
+import "./style.css";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { code, inputStates, inputClass, handleChange, handleKeyDown } =
+    useVerificationHook(6);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="layout">
+      <div className="container">
+        <h1>React Code Hook</h1>
+        <p>A simple React hook for capturing and verifying PIN codes</p>
+        <div className="btn-wrapper">
+          <a className="cta" href="#" target="_blank">
+            How to install
+          </a>
+        </div>
+        <div className="input-container">
+          <p>Demo</p>
+          <div className="flexed">
+            {inputStates.map((state, ii) => {
+              return (
+                <input
+                  type="number"
+                  value={state.digit}
+                  className={inputClass}
+                  onChange={(e) => handleChange(e, ii)}
+                  onKeyDown={handleKeyDown}
+                />
+              );
+            })}
+          </div>
+          <div className="code">
+            <p>
+              <b>Code:</b>{" "}
+              {code ? code : "Fill up the boxes to see the code here..."}
+            </p>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: resolve(__dirname, "./src/lib/useVerificationCode.ts"),
+      name: "useVerificationCode",
+    },
+    rollupOptions: {
+      external: ["react"],
+      output: {
+        globals: {
+          react: "React",
+        },
+      },
+    },
+  },
+});
